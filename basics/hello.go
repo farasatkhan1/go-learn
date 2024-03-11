5c1022f56777fd1749d9c627716228f17678ad54
Farasat Khan <farasat.khan@jintech.com>
2024-03-11 12:40:19 +0500

    moved to switch from if

--- basics/hello.go
+++ basics/hello.go
@@ -15,14 +15,16 @@ func hello(name string, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+		case spanish:
+			prefix = spanishHelloPrefix
+		case french:
+			prefix = frenchHelloPrefix
 	}
 
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
